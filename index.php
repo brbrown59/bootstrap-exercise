0cd5ff962b2d0b015fe2b70e23311572f8afd7c0
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-29 16:49:30 -0600

    Test to make sure bootstrap files properly linked, and created a styles.css

--- index.php
+++ index.php
@@ -25,6 +25,21 @@
 			<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
 					  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
 					  crossorigin="anonymous"></script>
-
+			<title>Bootstrap Example</title>
 		</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<p>Content!</p>
+				</div>
+				<div class="col-md-4">
+					<p>is!</p>
+				</div>
+				<div class="col-md-4">
+					<p>awesome!!!</p>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
