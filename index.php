1d9b6e610abaabb880f4da8fdb668b1251dd5a39
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-29 20:22:29 -0600

    fixed a really stupid syntax error

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
-	<html lang ="en">
-		<head>
-			<meta charset="utf-8"/>
-			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<!--bootstrap's css-->
-			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
-					integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
-					crossorigin="anonymous"/>
-			<!--optional-->
-			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
-					integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX"
-					crossorigin="anonymous"/>
-			<!--My CSS-->
-			<link rel="stylesheet" href="/styles.css"/>
-			<!--IE8 crosscompatibility-->
-			<!--[if lt IE 9]>
-				<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-				<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-			<![endif]-->
+<html lang ="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--bootstrap's css-->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+				integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+				crossorigin="anonymous"/>
+		<!--optional-->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+				integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX"
+				crossorigin="anonymous"/>
+		<!--My CSS-->
+		<link rel="stylesheet" href="/styles.css"/>
+		<!--IE8 crosscompatibility-->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
 
-			<!--jQuery-->
-			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<!--jQuery-->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
-			<!--bootstrap's JavaScript-->
-			<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
-					  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
-					  crossorigin="anonymous"></script>
-			<title>Bootstrap Example</title>
-		</head>
+		<!--bootstrap's JavaScript-->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
+				  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
+				  crossorigin="anonymous"></script>
+		<title>Bootstrap Example</title>
+	</head>
 	<body>
 		<header class="content-header">
 			<nav class="navbar">
@@ -43,7 +43,7 @@
 		</header>
 		<div class="container">
 			<div class="row">
-				<div class="column-md-12">
+				<div class="col-md-12">
 					<p class="lead-paragraph">This is some content.  It's robably not good content; I'm really just rambling to fill up space.  I mean,
 						I might put a few pictures in other containers just to test them, but this is merely a simple example project
 						and I don't want to go to extreme lengths when bootstrap is already giving me one heck of a challenge.  So,
@@ -51,6 +51,21 @@
 					</p>
 				</div>
 			</div>
+			<div class="row">
+				<div class="col-md-3">
+					<p>More meaningless, rambling text that's just here to fill space</p>
+				</div>
+				<div class="col-md-3">
+					<p>More meaningless, rambling text that's just here to fill space</p>
+				</div>
+				<div class="col-md-3">
+					<p>More meaningless, rambling text that's just here to fill space</p>
+				</div>
+				<div class="col-md-3">
+					<p>More meaningless, rambling text that's just here to fill space</p>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
+
