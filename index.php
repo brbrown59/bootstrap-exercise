30568e65f882c8125481fd8341249edc2968f6fe
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-29 19:31:41 -0600

    Added the first container below the navbar.

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 	<html lang ="en">
 		<head>
-			<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta charset="utf-8"/>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<!--bootstrap's css-->
 			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
 					integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
-					crossorigin="anonymous">
+					crossorigin="anonymous"/>
 			<!--optional-->
 			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
 					integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX"
-					crossorigin="anonymous">
+					crossorigin="anonymous"/>
+			<!--My CSS-->
+			<link rel="stylesheet" href="/styles.css"/>
 			<!--IE8 crosscompatibility-->
 			<!--[if lt IE 9]>
 				<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -31,13 +33,24 @@
 		<header class="content-header">
 			<nav class="navbar">
 				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 						<span class="sr-only">Toggle navigation</span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Brand</a>
+					<a class="navbar-brand" href="#">The Example</a>
 				</div>
 			</nav>
 		</header>
+		<div class="container">
+			<div class="row">
+				<div class="column-md-12">
+					<p class="lead-paragraph">This is some content.  It's robably not good content; I'm really just rambling to fill up space.  I mean,
+						I might put a few pictures in other containers just to test them, but this is merely a simple example project
+						and I don't want to go to extreme lengths when bootstrap is already giving me one heck of a challenge.  So,
+						you get these dry ramblings that you probably don't want to read.  Sorry.
+					</p>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
